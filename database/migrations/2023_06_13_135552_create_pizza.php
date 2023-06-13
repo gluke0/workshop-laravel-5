eecc56daf56d3f3f4864b695a5f71c0ffb25451c
Cicidong00 <136182931+Cicidong00@users.noreply.github.com>
2023-06-13 16:22:41 +0200

    table pizza

---
+++ database/migrations/2023_06_13_135552_create_pizza.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizza', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->text('description');
+            $table->decimal('price', $precision = 4, $scale = 2);
+            $table->smallInteger('calories');
+            $table->boolean('vegan')->default(false);
+            $table->boolean('available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizza');
+    }
+};
