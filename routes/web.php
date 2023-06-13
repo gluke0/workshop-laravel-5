d3394960ba20361a7cd2f5b51cfb3a7bd80bfc94
Luis-Mocha <luismocha97@gmail.com>
2023-06-13 16:36:52 +0200

    route welcome

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PizzaController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PizzaController::class, 'getPizza'])->name('welcome');
